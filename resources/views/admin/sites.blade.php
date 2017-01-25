105a750ef20349e51bf61e8f2c99848395e4014b
Elibool <elibool@outlook.com>
2017-01-25 14:42:12 +0800

    View 视图模板文件

--- resources/views/admin/sites.blade.php
+++ resources/views/admin/sites.blade.php
@@ -7,7 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>yi admin</title>
-
     <!-- Bootstrap -->
     <link href="{{ $cdn_path }}/admin/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
@@ -46,7 +45,7 @@
                 <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
                     <div class="menu_section active">
                         <h3>General</h3>
-                        <ul class="nav side-menu" >
+                        <ul class="nav side-menu">
                             @foreach($permission_tree as $info)
                                 <li>
                                     <a class="menu_tile"><i class="{{ $info['icon'] }}"></i> {{ $info['name'] }} <span class="fa fa-chevron-down"></span></a>
