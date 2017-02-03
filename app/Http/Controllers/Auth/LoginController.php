48140c0164aabe705aeaf94d298ae867d532dfd1
Elibool <elibool@outlook.com>
2017-02-03 19:21:39 +0800

    Laravel 自带登录控制器

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -24,7 +24,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/admin/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
